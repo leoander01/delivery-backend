c409873154540cd5ef4364d08062d175c2589166
leoander01 <leoanderneves8@outlook.com>
2022-10-20 08:32:34 -0300

    Cadastro de cliente

---
+++ src/modules/clients/usecases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,35 @@
+import { prisma } from '../../../../database/prismaClient'
+import { hash } from 'bcrypt'
+
+interface ICreateClient {
+  username: string
+  password: string
+}
+
+export class CreateClientUseCase {
+  async execute ({ username, password }: ICreateClient) {
+    // Validar se o 'client' existe
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: 'insensitive'
+        },
+      },
+    })
+
+    if (clientExist) {
+      throw new Error('Client already exists')
+    }
+    // Criptografar a senha
+    const hashPassword = await hash(password, 10)
+    // Salvar o client
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    })
+
+    return client
+  }
+}
